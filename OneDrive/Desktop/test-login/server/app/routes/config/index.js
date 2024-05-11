5220252096a3e67b985e460e0411d9c3e06978bb
Mark Dinglasa <ryanmark.dinglasa@gmail.com>
2024-05-11 17:14:48 +0800

    Delete OneDrive/Desktop/test-login directory

--- OneDrive/Desktop/test-login/server/app/routes/config/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express"), d72746572 = express.Router(), r636E6667 = require("../../controllers/config");
-
-d72746572.post("/connect", r636E6667.k636F6E6E); 
-d72746572.post("/license-key", r636E6667.k6C636E736B79); 
-d72746572.get("/get-key", r636E6667.k67746B74);
-d72746572.post("/get-license", r636E6667.k67746C6E6373);
-
-module.exports = d72746572;
--- OneDrive/Desktop/test-login/server/app/routes/test/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express"), d72746572 = express.Router(), { checkUser, requireAuth } = require('../../middleware/authMiddleware'), r617468 = require("../../controllers/auth");
-const test = require('../../controllers/test')
-
-d72746572.get("/", test.testing ); 
-d72746572.post("/register", test.register);
-d72746572.post("/pass", test.pass);
-
-module.exports = d72746572;
