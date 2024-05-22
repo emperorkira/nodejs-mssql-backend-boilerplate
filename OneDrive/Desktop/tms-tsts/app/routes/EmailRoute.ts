f37b172075fc66cc089880c8fe3e5ba52f495d53
Mark Dinglasa <ryanmark.dinglasa@gmail.com>
2024-05-22 09:17:40 +0800

    Delete OneDrive/Desktop/tms-tsts directory

--- OneDrive/Desktop/tms-tsts/app/routes/EmailRoute.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { sendEmail, verifyToken } from '..'
-
-const router = express.Router()
-router.post("/sendemail", verifyToken, sendEmail)
-
-export default router
--- OneDrive/Desktop/tms-tsts/app/schemas/TicketLineSchema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi'
-
-export const ticketLineSchema = Joi.object({
-  TicketId: Joi.number().integer().allow(null), // FK of TicketSchema
-  Action: Joi.string().allow(null),
-  DateCalled: Joi.date().allow(null),
-  DateFinished: Joi.date().allow(null),
-});
